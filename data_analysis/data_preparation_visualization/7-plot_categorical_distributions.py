29efdce634bc1005e6cb644659b5844d953600df
Alejandro <10653@holbertonstudents.com>
2026-03-14 18:37:31 -0300

    Task 7: use plt.subplot grid to match reference layout

--- data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
+++ data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
@@ -30,16 +30,12 @@ def plot_categorical_distributions(df, columns_to_plot=None):
     n_cols = 3
     n_rows = (n_features + n_cols - 1) // n_cols
 
-    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5 * n_rows))
-    axes = np.array(axes).reshape(-1)
+    plt.figure(figsize=(15, 5 * n_rows))
 
-    for i, col in enumerate(columns_to_plot):
-        ax = axes[i]
-        df[col].value_counts().plot(kind='bar', ax=ax, rot=45)
-        ax.set_title(col)
-
-    for j in range(n_features, len(axes)):
-        axes[j].axis('off')
+    for i, col in enumerate(columns_to_plot, start=1):
+        plt.subplot(n_rows, n_cols, i)
+        df[col].value_counts().plot(kind='bar', rot=45)
+        plt.title(col)
 
     plt.tight_layout()
     plt.savefig("Task_7.png")
