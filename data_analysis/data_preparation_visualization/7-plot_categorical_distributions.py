659f68aba80b1571d01aa2e5089f69d489fb0b3a
Alejandro <10653@holbertonstudents.com>
2026-03-16 16:15:08 -0300

    fix(task_7): use set_visible(False) for empty subplots instead of delaxes to pass checker comparison

--- data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
+++ data_analysis/data_preparation_visualization/7-plot_categorical_distributions.py
@@ -3,7 +3,6 @@
 Module to plot categorical distributions
 """
 import matplotlib.pyplot as plt
-import math
 
 
 def plot_categorical_distributions(df, columns_to_plot=None):
@@ -17,10 +16,10 @@ def plot_categorical_distributions(df, columns_to_plot=None):
     else:
         columns_to_plot = list(columns_to_plot)
 
-    n_cols, n_rows = 3, math.ceil(len(columns_to_plot) / 3)
-
+    n_cols, n_rows = 3, (len(columns_to_plot) + 2) // 3
+   
     fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5 * n_rows))
-
+   
     if isinstance(axes, plt.Axes):
         axes = [axes]
     else:
@@ -28,13 +27,12 @@ def plot_categorical_distributions(df, columns_to_plot=None):
 
     for i, col in enumerate(columns_to_plot):
         counts = df[col].value_counts()
-        axes[i].bar(counts.index.astype(str), counts.values)
+        axes[i].bar(counts.index, counts.values)
         axes[i].set_title(col)
         axes[i].tick_params(axis='x', rotation=45)
 
-    # Ocultar (no eliminar) los subplots que sobran
     for j in range(len(columns_to_plot), len(axes)):
-        axes[j].axis('off')
+        axes[j].set_visible(False)
 
     plt.tight_layout()
     plt.savefig("Task_7.png")
