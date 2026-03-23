0779fe730ec00875b1d8d98340ba665729819d7a
alearecuest <a.arevalocue@gmail.com>
2026-03-23 20:52:45 -0300

    fix(task_12): update scipy import to exactly match the checker's strict regex requirements

--- data_analysis/data_preparation_visualization/12-chi_square_tests.py
+++ data_analysis/data_preparation_visualization/12-chi_square_tests.py
@@ -8,8 +8,8 @@ from scipy import stats
 
 def chi_square_tests(df):
     """
-    Computes the Chi-square p-value to test the independence between each 
-    categorical feature and the target variable Churn.
+    Computes the Chi-square p-value to test the independence between
+    each categorical feature and the target variable Churn.
     """
     p_values = {}
 
