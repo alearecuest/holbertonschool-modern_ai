8af40a82de6d7d4fa4abacccc153b6eea5c4a8b1
alearecuest <a.arevalocue@gmail.com>
2026-03-23 20:37:28 -0300

    feat(task_12): compute chi-square p-values dynamically catching both object and discrete numeric columns like SeniorCitizen

---
+++ data_analysis/data_preparation_visualization/12-chi_square_tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module to perform chi-square tests
+"""
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+
+def chi_square_tests(df):
+    """
+    Computes the Chi-square p-value to test the independence between
+    each categorical feature and the target variable Churn.
+    """
+    p_values = {}
+
+    for col in df.columns:
+        if col in ['Churn', 'customerID']:
+            continue
+
+        if df[col].dtype == 'object' or df[col].nunique() < 10:
+            contingency_table = pd.crosstab(df[col], df['Churn'])
+
+            chi2, p, dof, expected = chi2_contingency(contingency_table)
+
+            p_values[col] = p
+
+    return p_values
